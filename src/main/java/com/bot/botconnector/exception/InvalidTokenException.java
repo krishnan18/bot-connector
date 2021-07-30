a33a8f038a934cb7a2bcaaaa2fce77c03e69dee5
BalaKrishnan Rangasamy <balakrishnan.rangasamy@rabobank.nl>
2021-07-30 16:14:40 +0530

    added token validation

---
+++ src/main/java/com/bot/botconnector/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.bot.botconnector.exception;
+
+public class InvalidTokenException extends Exception {
+    public InvalidTokenException() {
+        super("Invalid Token");
+    }
+}
--- src/main/java/com/bot/botconnector/interceptor/BotConnectorInterceptor.java
+++ src/main/java/com/bot/botconnector/interceptor/BotConnectorInterceptor.java
@@ -1,9 +1,11 @@
 package com.bot.botconnector.interceptor;
 
+import com.bot.botconnector.exception.InvalidTokenException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.naming.directory.InvalidSearchControlsException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +15,11 @@ public class BotConnectorInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        var xAuthSecret = request.getHeader("x-auth");
+        if(!"test".equals(xAuthSecret)) {
+            log.error("invalid Token");
+            throw new InvalidTokenException();
+        }
         log.info("Request Method: {}",request.getMethod());
         log.info("Request Content Type: {}",request.getContentType());
         log.info("Request Headers: {}",request.getHeaderNames());
